d2255d797fb7f3421a1a6c0f87f650072f9b776e
shefila <shefila.rizqi@gmail.com>
2022-06-29 19:24:45 +0700

    tsk 6

--- app/Http/Controllers/SavingController.php
+++ app/Http/Controllers/SavingController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class SavingController extends Controller
 {
-    //
+    public function index()
+    {
+    return view('saving.index', compact('saving'));
+    }
 }
---
+++ resources/views/saving/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.user')
+
+@section('title') Order @stop
+
+@section('content')
+    <section class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <h1>Saving Your Money</h1>
+                        <a href="#" class="btn btn-success">Saving Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">My Saving</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>Saldo</th>
+                                <th>Status</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($orders as $order)
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>
+                                        <a href="#" class="btn btn-info btn-sm"><i class="fas fa-eye"></i></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </section>
+@stop
