d0e491605ca3d0b0727b04ba1bd7d5d46b38cd96
shefila <shefila.rizqi@gmail.com>
2022-06-29 19:13:46 +0700

    tsk 6

--- database/migrations/2022_06_29_094432_create_savings_table.php
+++ database/migrations/2022_06_29_094432_create_savings_table.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 
 class CreateSavingsTable extends Migration
 {
