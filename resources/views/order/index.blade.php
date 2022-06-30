e42ace57e3e6a862650cd845709a02832173b39d
shefila <shefila.rizqi@gmail.com>
2022-06-30 12:17:18 +0700

    tsk done

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -26,7 +26,7 @@
                         <table id="example1" class="table table-bordered table-striped">
                             <thead>
                             <tr>
-                                <th>Shipping Address</th>
+                                <th>Address</th>
                                 <th>Total item</th>
                                 <th>Option</th>
                                 <th>Amount</th>
