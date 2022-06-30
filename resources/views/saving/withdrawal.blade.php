9f433446bdc35457d117aec6bc59d42cf8b98bcd
shefila <shefila.rizqi@gmail.com>
2022-06-30 08:21:03 +0700

    tsk 7

--- resources/views/saving/withdrawal.blade.php
+++ resources/views/saving/withdrawal.blade.php
@@ -14,6 +14,9 @@
 
                 <div class="card">
                     <div class="card-body">
+                        <button class="btn btn-success" data-toggle="modal" data-target="#addWithdrawal"><i class="fas fa-plus"></i>
+                            Withdrawal
+                        </button>
                         <div class="row">
                             <div class="col-12">
                                 <h2>Saldo : </h2>
@@ -48,4 +51,32 @@
         </div>
         <!-- /.row -->
     </section>
+
+    <div id="addSaldo" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Withdrawal</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <form action="#" method="POST">
+                    <div class="modal-body">
+                        @csrf
+                        <div class="form-group">
+                            <label>Amount Withdrawal</label>
+                            <input type="number" class="form-control" name="withdrawal" required>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-success">Submit</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
 @stop
